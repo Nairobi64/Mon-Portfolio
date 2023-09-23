81c415b8971905d4d115f5a5b7ee03285c1cb896
Nairobi64 <110598402+Nairobi64@users.noreply.github.com>
2023-09-23 23:09:43 +0000

    Mise a jour du projet

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,15 @@
-// code de la app,js
-// code de la app,js
-// code de la app,js
+ // Bloque l'action par defaut du formulaire
 
-const express = require ('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (event)=> {
+ 
+   // recuperation des elements du formulaire
+ 
+   let baliseNom = document.getElementById('firstname')
+   let userName = baliseNom.value
+ 
+   console.log(userName)
+ 
+ })
 
-
-const usersRoutes = require ('./routes/users');
-const userRouter = require ('./routes/rUsers');
-
-
-// declaration de la connexion a la base de donnee.
-mongoose.connect('mongodb+srv://reg-creator:Reg-BD24@cluster0.5wp9m7l.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-  const app = express();
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-   // Définir le répertoire statique pour les fichiers frontend
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));;
-
-app.use('/api/users', usersRoutes);
-app.use('api/auth', userRouter);
-
-module.exports = app;
+    
