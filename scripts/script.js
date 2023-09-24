19f56c7d3bd57b4fe7e7983da3598a8c315c2217
Nairobi64 <110598402+Nairobi64@users.noreply.github.com>
2023-09-24 00:10:54 +0000

    Mise a jour du fichier script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,27 @@
- // Bloque l'action par defaut du formulaire
+// Afficher l'email
 
- let form = document.querySelector('form')
- form.addEventListener('submit', (event)=> {
- 
-   // recuperation des elements du formulaire
- 
-   let baliseNom = document.getElementById('firstname')
-   let userName = baliseNom.value
- 
-   console.log(userName)
- 
- })
+function afficherEmail(nom, email, Message) {
+  let mailto = `mailto:${email}?${firstname} ${Message} `
+  location.href = mailto
+}
 
-    
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    // Bloque l'action par défaut du formulaire
+    event.preventDefault();
+
+    // Récupération des éléments du formulaire
+    let baliseNom = document.getElementById('firstname');
+    let userName = baliseNom.value;
+
+    let baliseEmail = document.getElementById('Email')
+    let UserEmail = baliseEmail.value
+
+    let BaliseMessage = document.getElementById('Message')
+    let UserMessage = BaliseMessage.value
+
+    afficherEmail(firstname, Email, Message)
+
+    // Vous pouvez faire d'autres opérations ici, comme envoyer les données via une requête AJAX, par exemple.
+});
